b6c2dd08a253b07cfc8deb838fabe94d48becc81
Dreikor17 <178236069+Dreikor17@users.noreply.github.com>
2026-06-14 20:46:24 -0500

    Bump dev to 0.2.5-dev after the v0.2.4 release

--- nordic_ota_flasher/__init__.py
+++ nordic_ota_flasher/__init__.py
@@ -1,3 +1,3 @@
-"""Nordic nRF52 BLE OTA flasher for MeshCore (RAK4631 / Adafruit + OTAFIX bootloader)."""
+﻿"""Nordic nRF52 BLE OTA flasher for MeshCore (RAK4631 / Adafruit + OTAFIX bootloader)."""
 
-__version__ = "0.2.4"
+__version__ = "0.2.5-dev"
